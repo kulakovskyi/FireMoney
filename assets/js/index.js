fd9c28d216a44fd25f23ad5c50a269c46dc537af
Andriy <81190009+kulakovskyi@users.noreply.github.com>
2022-06-11 17:49:22 +0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,63 @@
 console.log("Работает только на индексеі");
 
+//burger menu
+
+const iconMenu = document.querySelector(".menu__icon");
+const iconMenuSpan = document.querySelector(".menu__icon > span");
+const menuBody = document.querySelector(".header__menu");
+
+iconMenu.addEventListener("click", function (e) {
+  iconMenu.classList.toggle("_active");
+  menuBody.classList.toggle("_active");
+
+  // заекрытие меню по клику на любую область
+  // дополниельно делаем проверку клик по спану, что бы отрабатывало закрытие
+  window.onclick = function (e) {
+    if (e.target !== menuBody && e.target !== iconMenu && e.target !== iconMenuSpan) {
+      menuBody.classList.remove("_active");
+      iconMenu.classList.remove("_active");
+    }
+  };
+});
+
+//появление шапки про скролле вниз
+
+let lastScroll = 0;
+const defaultOffset = 200;
+const header = document.querySelector(".header");
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains("_hide");
+
+window.addEventListener("scroll", () => {
+  if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+    //scroll down
+    header.classList.add("_hide");
+  } else if (scrollPosition() < lastScroll && containHide()) {
+    //scroll up
+    header.classList.remove("_hide");
+  }
+
+  lastScroll = scrollPosition();
+});
+
+// скролл по якорю
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (const anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const blockID = anchor.getAttribute("href");
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+/********************************/
+
 const toolBack = document.querySelector(".tooltip__back");
 const toolPrice = document.querySelector(".tooltip__price-mounth");
 
